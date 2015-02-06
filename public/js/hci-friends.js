6acdd2a47052561b99bbe18d044d16d145c3b3a5
vagrant <vagrant@precise32.(none)>
2015-02-06 20:02:34 +0000

    final commit

--- public/js/hci-friends.js
+++ public/js/hci-friends.js
@@ -10,6 +10,12 @@ $(document).ready(function() {
  */
 function initializePage() {
 	console.log("Javascript connected!");
+	$(".nameLink").click(listenerFunction);
+}
+
+function listenerFunction(e) {
+	e.preventDefault();
+	$(this).text(anagrammedName($(this).text()));
 }
 
 function anagrammedName(name) {
